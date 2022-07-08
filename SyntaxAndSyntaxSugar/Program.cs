e1a7243cd67bde4411439813ab25d14508cb8be4
cpheighter <cheighterjr@gmail.com>
2022-07-08 09:27:51 -0400

    init

---
+++ SyntaxAndSyntaxSugar/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Orginal Code
+int answer = 4;
+string response;
+if (answer < 9)
+{
+    response = answer + " is less than nine";
+}
+else
+{
+    response = answer + " is greater than or equal to nine";
+}*/
+
+//CCR code
+int answer = 4;
+string response = answer < 9 ?$"{answer} is less than nine" :$"{answer} is greater than or equal to nine";
+Console.WriteLine(response);
